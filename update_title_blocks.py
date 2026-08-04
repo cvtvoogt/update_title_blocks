5fce2949f9f8d7e86f4f2bc36061c968b260b1fa
cvtvoogt <corne@cmvoogt.nl>
2026-08-04 12:16:46 +0200

    Update update_title_blocks.py

--- update_title_blocks.py
+++ update_title_blocks.py
@@ -1,13 +1,14 @@
 import wx
 import pcbnew
 
-class HelloWorldPlugin(pcbnew.ActionPlugin):
+class UpdateTitleBlocksPlugin(pcbnew.ActionPlugin):
     def defaults(self):
-        self.name = "Hello World"
-        self.category = "Hello World"
+        self.name = "Update Title Blocks"
+        self.category = "Update Title Blocks"
         self.description = "Simple Hello World plugin for KiCad 10 schematic"
         self.show_toolbar_button = True
-        self.icon_file_name = "update_title_blocks.png"
+        self.icon_file_name = "hello_icon.png"
 
     def Run(self):
-        wx.MessageBox("Hello World from KiCad 10 Schematic Plugin!", "Hello World")
+        wx.MessageBox("Hello World from Update Title Blocks!", "Update Title Blocks")
+
